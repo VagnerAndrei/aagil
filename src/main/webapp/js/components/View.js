f908c8acba15e105a749930ee1f68383ff9cf72c
Vagner <Vagner@Vagner>
2021-04-01 12:18:20 -0300

    Merge

--- src/main/webapp/js/components/View.js
+++ src/main/webapp/js/components/View.js
@@ -4,17 +4,12 @@
 export class View {
 
 	constructor(titulo) {
-<<<<<<< HEAD
 		this._titulo = titulo
 		document.getElementsByTagName('main')[0].innerHTML = this.template()
 		document.getElementsByTagName('title')[0].innerHTML = this._titulo;
-=======
-		document.getElementsByTagName('main')[0].innerHTML = this.template()
-		document.getElementsByTagName('title')[0].innerHTML = titulo;
->>>>>>> branch 'main' of https://github.com/VagnerAndrei/aagil
 	}
-	
-	template(){
+
+	template() {
 		throw new Error('Not Yet Implemented')
 	}
 
